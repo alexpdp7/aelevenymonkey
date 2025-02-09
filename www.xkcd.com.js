f5f1b68f7167358ee114b2e8e0ba95d9983790b3
alex <alex@pdp7.net>
2025-02-09 12:37:04 +0100

    Fix link for XKCD front page

--- www.xkcd.com.js
+++ www.xkcd.com.js
@@ -2,7 +2,7 @@ if (document.URL.startsWith("https://www.xkcd.com") || document.URL.startsWith("
     if (document.URL.endsWith("xkcd.com/")) {
 	// last strip
 	prev = document.getElementsByClassName("comicNav")[0].children[1].children[0].attributes.href.nodeValue.replaceAll("/", "");
-	index = Number.parseInt(prev);
+	index = Number.parseInt(prev) + 1;
     }
     else {
 	index = Number.parseInt(document.URL.slice(document.URL.search("xkcd.com") + 9).replace("/", "")); 
